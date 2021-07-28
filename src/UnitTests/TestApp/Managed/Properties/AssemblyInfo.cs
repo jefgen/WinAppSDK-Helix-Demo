a0b22b28d9c796cbe19f281e331a05ffc4fb3477
Jeff Genovy <29107334+jefgen@users.noreply.github.com>
2021-07-27 18:25:48 -0700

    Add a simple TAEF test app.

---
+++ src/UnitTests/TestApp/Managed/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All Rights Reserved.
+//-----------------------------------------------------------------------------
+
+using System;
+using System.Resources;
+
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: CLSCompliant(true)]
---
+++ src/UnitTests/TestApp/Managed/TestClass1.cs
@@ -0,0 +1,27 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All Rights Reserved.
+//-----------------------------------------------------------------------------
+
+using WEX.TestExecution;
+using WEX.TestExecution.Markup;
+
+namespace TestApp.Managed.UnitTests
+{
+    [TestClass]
+    public class TestClass1
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Log.Comment("Hello World!");
+			Verify.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+			Log.Comment("Test2");
+            Verify.AreEqual(2, 2);
+        }
+    }
+}
